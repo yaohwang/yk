8fc871826bd71502bfd8a8ab24506c3adc5769d1
yaohwang <yaohwang@163.com>
2021-06-17 17:03:07 +0800

    refactor(*): move outerside to .archive and refresh api module

--- api.py
+++ api.py
@@ -3,8 +3,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-from classify_chat.api import ads_bp
+from ads.api import ads_bp
 app.register_blueprint(ads_bp)
 
-from cluster_comment_taptap.api import topic_bp
+# from ads_sgz2017.api import ads_sgz2017_bp
+# app.register_blueprint(ads_sgz2017_bp)
+
+from topic_modeling.api import topic_bp
 app.register_blueprint(topic_bp)
