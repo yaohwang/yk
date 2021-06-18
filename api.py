1d7e5f336e807e39d2c92f61701d4bbf293a217e
yaohwang <yaohwang@163.com>
2021-06-18 14:30:00 +0800

    refactor(ads): remove sgz2017 in api

--- api.py
+++ api.py
@@ -6,8 +6,5 @@ app = Flask(__name__)
 from ads.api import ads_bp
 app.register_blueprint(ads_bp)
 
-# from ads_sgz2017.api import ads_sgz2017_bp
-# app.register_blueprint(ads_sgz2017_bp)
-
 from topic_modeling.api import topic_bp
 app.register_blueprint(topic_bp)
